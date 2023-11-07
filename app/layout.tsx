3096f40e771140ce63108d11168eea2b727d0c85
Pablo Cigoy <pablocigoy@gmail.com>
2023-11-07 23:19:50 +0100

    images and tailwind conig

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import { monserrat } from "./ui/fonts";
+import "./ui/global.css";
+export default function RootLayout({children}: {children: React.ReactNode}) {
+    return (
+        <html lang="en">
+            <body className={`${monserrat.className} antialiased`}>
+                {children}
+                </body>
+        </html>
+    );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Lusitana, Monsieur_La_Doulaise, Montserrat} from "next/font/google";
+
+export const monserrat = Montserrat({subsets: ["latin"]});
+
+export const lusitana = Lusitana({
+    weight: ["400"],
+    subsets: ["latin"],
+});
