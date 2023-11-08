b9d0b082c8a40d7972d093d88dbb585f6ca39c0e
Pablo Cigoy <pablocigoy@gmail.com>
2023-11-08 22:39:43 +0100

    navbar and buttons

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Todo esto es el contenido del dashboard</p>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <html lang="en">
             <body className={`${monserrat.className} antialiased`}>
                 {children}
+                <footer className="py-10 flex justify-center items-center">Hecho con ❤️ por mi</footer>
                 </body>
         </html>
     );
