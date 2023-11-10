c4074164cfa79e975cf5ebacad1dec2818922df0
Pablo Cigoy <pablocigoy@gmail.com>
2023-11-10 23:09:22 +0100

    columns

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,12 +5,22 @@ export default function Customers() {
         <h1 className={` mb-4 text-xl md:text-2xl`}>
             Dashboard
         </h1>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-5">
             segundo
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            Primera column
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-            hola
+            Segunda column
         </div>
+        <div className="mt-6 grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            tercera column
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            cuarta Colunm
+        </div>
+        </div>
+        
     </main>
           );
     
