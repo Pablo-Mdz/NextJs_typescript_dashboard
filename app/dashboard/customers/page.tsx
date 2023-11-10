5733a29e1ee62a3d5b1bafb729f120c95cb70854
Pablo Cigoy <pablocigoy@gmail.com>
2023-11-10 23:02:03 +0100

    costumers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
 export default function Customers() {
-    return <p>Costumers...</p>
+    return (
+        
+        <main>
+        <h1 className={` mb-4 text-xl md:text-2xl`}>
+            Dashboard
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            segundo
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            hola
+        </div>
+    </main>
+          );
+    
 }
